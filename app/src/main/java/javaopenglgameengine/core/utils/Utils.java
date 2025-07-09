67e2d06b1263ea486d2544e8e782d4e3af0a9263
Tilen Smrdel <69430904+TilenS2002@users.noreply.github.com>
2025-07-09 10:57:00 +0200

    OBJ loader

--- app/src/main/java/javaopenglgameengine/core/utils/Utils.java
+++ app/src/main/java/javaopenglgameengine/core/utils/Utils.java
@@ -1,10 +1,17 @@
 package javaopenglgameengine.core.utils;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import javax.swing.text.BoxView;
 
 import org.lwjgl.system.MemoryUtil;
 
@@ -29,5 +36,20 @@ public class Utils {
             result = scanner.useDelimiter("\\A").next();
         }
         return result;
+    }
+
+    public static List<String> readAllLines(String fileName) {
+        List<String> list = new ArrayList<>();
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(Class.forName(Utils.class.getName()).getResourceAsStream(fileName)))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                list.add(line);
+            }
+        }
+        catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return list;
     }
 }
